49477da31daebfbad99347b72dcb15a5f8ad8890
Carlos Martinez <Carlos.martdelgado@gmail.com>
2022-03-21 11:18:51 +0100

    working with the services2

--- models/User.model.js
+++ models/User.model.js
@@ -3,10 +3,7 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      unique: true,
-    },
+    username: String,
     email: {
       type: String,
       unique: true,
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,56 +3,67 @@ const UserModel = require("../models/User.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+// Require necessary (isLoggedOut and isLoggedIn) middleware in order to control access to specific routes
+const isLoggedIn = require("../middleware/isLoggedIn");
+
 // Signup
+
 router.post("signup", async (req, res, next) => {
   const { username, email, password, city, address, postCode } = req.body;
 
   if (!username || !email || !password || !city || !address || !postCode) {
-    res.status(400).json({ errorMessage: "Llenar todos los campos" });
+    res.status(400).json({ errorMessage: "Rellenar todos los campos" });
     return;
   }
 
-  // aqui aplicamo todos nuestros validadores de BE
+  const passwordRegexp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
+
+  if (!passwordRegexp.test(password)) {
+    res.status(400).json({
+      errorMessage:
+        "Password has to be between 8 & 15, capitalize letter, lowercase letter, one digit and at least one special character",
+    });
+    return;
+  }
+
+  // Check email
 
   try {
-  // chequear si el usuario ya existe
-  const foundUser = await UserModel.findOne({ email });
-  if (foundUser) {
-    res.status(400).json({ errorMessage: "Usuario ya existe" });
-    return;
+    const foundUser = await UserModel.findOne({ email });
+    if (foundUser) {
+      res.status(400).json({ errorMessage: "Usuario ya existe" });
+      return;
+    }
+
+    // Aqui si todo lo anterior va bien, crearemos el usuario
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await UserModel.create({
+      username,
+      email,
+      password: hashedPassword,
+      city,
+      address,
+      postCode,
+      role,
+    });
+
+    res.status(201).json();
+  } catch (err) {
+    next(err);
   }
-
-  // aqui si todo lo anterior va bien, crearemos el usuario
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  await UserModel.create({
-    username,
-    email,
-    password: hashedPassword,
-    city,
-    address,
-    postCode,
-    role,
-  });
-
-  res.status(201).json();
-
-  } catch(err) {
-      next(err)
-  }
-
-
 });
 
 // Login
 
 router.post("/login", async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
- // Validacione de BE
+  // Validacione de BE
 
- if (!email || !password) {
+  if (!email || !password) {
     res.status(400).json({ errorMessage: "Llenar todos los campos" });
     return;
   }
@@ -76,7 +87,6 @@ router.post("/login", async (req, res, next) => {
       return;
     }
 
-    
     // Aqui creamos el token y lo enviamos
 
     const payload = {
@@ -91,11 +101,9 @@ router.post("/login", async (req, res, next) => {
       expiresIn: "6h",
     });
 
-    res.status(200).json({authToken})
-
+    res.status(200).json({ authToken });
   } catch (err) {
     next(err);
   }
-
-})
+});
 module.exports = router;
